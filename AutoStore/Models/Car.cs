86f3fdf9f1a3154046d2e894f5ef8387d0c6c61b
unknown <fouad.th@gmail.com>
2018-08-05 20:10:25 +0300

    first commit

---
+++ AutoStore/Models/Car.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoStore.Models
+{
+    public class Car
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string BodyStyle { get; set; }
+        public string Engine { get; set; }
+        public string Transmission { get; set; }
+        public string DriveTrain { get; set; }
+        public string Extcrior { get; set; }
+        public string Interior { get; set; }
+        public string Miles { get; set; }
+        public string  Doors { get; set; }
+        public string Passengers { get; set; }
+        public string Stock { get; set; }
+        public string Vw { get; set; }
+        public string FuelMilage { get; set; }
+        public string FuelType { get; set; }
+        public string Condition { get; set; }
+        public string Warranty { get; set; }
+        public string VechicleOverView { get; set; }
+        public string Cylinders { get; set; }
+        public string Displacement { get; set; }
+        public string EngineLayout { get; set; }
+        public string HoursePower { get; set; }
+        public string Rpm { get; set; }
+        public string Torqme { get; set; }
+        public string CompessionRatio { get; set; }
+        public string TopTrackSpeed { get; set; }
+        public string Mph_0_60 { get; set; }
+        public string ManualGearBox { get; set; }
+        public string City { get; set; }
+        public string TimeTo100 { get; set; }
+        public string Length { get; set; }
+        public string Width { get; set; }
+        public string Height { get; set; }
+        public string WheelHuse { get; set; }
+        public string MaximumPayload { get; set; }
+        public string CarbWeight { get; set; }
+        //luggage campul
+        public string FuelTankCapcity { get; set; }
+        public string Location { get; set; }
+        public bool AdaptiveCruiseControl { get; set; }
+        public bool Airbags { get; set; }
+        public bool Air_Conditioning { get; set; }
+        public bool AlarmSystem { get; set; }
+        public bool Anti_theftProtection { get; set; }
+        public bool AudioInterface { get; set; }
+        public bool AutomaticClimateControl { get; set; }
+        public bool AutomaticHeadlights { get; set; }
+        public bool AutoStartStop { get; set; }
+        public bool BiXenonHeadlights { get; set; }
+        public bool BluetoothHandset { get; set; }
+        public bool BoseSurroundSound { get; set; }
+        public bool BurnmesterSurroundSound { get; set; }
+        public bool CdDvdAutoChanger { get; set; }
+        public bool CdrAudio { get; set; }
+        public bool CruiseControl { get; set; }
+        public bool DirectFuelInjection { get; set; }
+        public bool ElectricParkingBrake { get; set; }
+        public bool FloorMats { get; set; }
+        public bool GarageDoorOpener { get; set; }
+        public bool LeatherPackage { get; set; }
+        public bool LockingRearDifferential { get; set; }
+        public bool LuggageComparments { get; set; }
+        public bool ManualTransmission { get; set; }
+        public bool NavigationModule { get; set; }
+        public bool OnlineServices { get; set; }
+        public bool ParkAssist { get; set; }
+        public bool porscheCommunication { get; set; }
+        public bool PowerSteering { get; set; }
+        public bool ReversingCamera { get; set; }
+        public bool RollOverProtection { get; set; }
+        public bool SeatHeating { get; set; }
+        public bool SeatVentilation { get; set; }
+        public bool SoundPackagePlus { get; set; }
+        public bool SportChronoPackage { get; set; }
+        public bool SteeringWheelHeating { get; set; }
+        public bool TirePressureMonitoring { get; set; }
+        public bool UniversalAudioInterface { get; set; }
+        public bool VoiceControlSystem { get; set; }
+        public bool WindDeflector { get; set; }
+    }
+}
---
+++ AutoStore/Models/Gallery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoStore.Models
+{
+    public class Gallery
+    {
+        public int Id { get; set; }
+        public string ImagePath { get; set; }
+        //car object
+
+    }
+}
